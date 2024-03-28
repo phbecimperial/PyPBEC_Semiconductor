Fixed absorption and emission rate calculation

PyPBEC/OpticalMedium.py
@@ -7,6 +7,7 @@ import pandas as pd
 import sys
 import os
 import csv
+from scipy.integrate import quad
 
 class OpticalMedium():
 
@@ -180,16 +181,26 @@ class InGaAs_QW(OpticalMedium):
 		interpolated_emission_spectrum = interp1d(emission_spectrum[0, :], emission_spectrum[1, :], kind='cubic')
 
 		# Uses both datasets
-		if np.min(1e9 * np.array(lambdas)) < 900 or np.max(1e9 * np.array(lambdas)) > 970:
-			raise Exception('*** Restrict wavelength to the range between 900 and 970 nm ***')
+		if np.min(1e9 * np.array(lambdas)) < 900 or np.max(1e9 * np.array(lambdas)) > 969:
+			raise Exception('*** Restrict wavelength to the range between 900 and 969 nm ***')
 
 		temperature = 300
-		cavity_lengths = lambdas*mode/2
 
-		emission_rates = interpolated_emission_spectrum(lambdas*1e9)
-		absorption_spectrum = interpolated_absorption_spectrum(lambdas*1e9)
-		absorption_time = 2*cavity_lengths*absorption_spectrum/sc.c #Average time taken to absorb one photon.
-		absorption_rates = 1/absorption_time
+		#Create function to calculate absorption rates
+		def absorption_rates_func(lamb, mode):
+			cavity_length = lamb * mode / 2
+			abs_time = 2*cavity_length/(sc.c*interpolated_absorption_spectrum(lamb))
+			absorption_rate = 1/abs_time
+			return absorption_rate
+
+		#Normalise emission rates to absorption
+		abs_sum, _ = quad(absorption_rates_func, 900, 969, args=(mode,))
+		emi_sum, _ = quad(interpolated_emission_spectrum, 900, 969)
+		sf = abs_sum/emi_sum
+
+		emission_rates = interpolated_emission_spectrum(lambdas*1e9)*sf
+		absorption_rates = absorption_rates_func(lambdas*1e9, mode)
+
 
 
 		return absorption_rates, emission_rates

Semiconductor_test.py
@@ -12,12 +12,13 @@ grid_size = 40.0*1e-6     # meters
 grid_delta = 0.5*1e-6      # meters
 q = 17                     # longitudinal mode number
 lamb0 = 950e-9             #meters
-L0 = lamb0 * q / 2         #cavity length
 
-n = 1
-n_modes = 100
+n = 1*2.5
+L0 = lamb0 * q / (2*n)       #cavity length, reconstructed to give us correct wavelengths.
+
+n_modes = 50
 feature_RoC = 100*1e-6   # This is 0.1 meters. Who knows why
-feature_depth = 1 # meters
+feature_depth = 0.1 # meters
 
 from PyPBEC.Cavity import Modes
 cavity_modes = Modes(grid_size=grid_size, grid_delta=grid_delta, L0=L0, q=q, n=n, n_modes=n_modes)
@@ -33,7 +34,7 @@ X, Y = cavity_modes.get_cavity_grid()
 pump_base = np.exp(-((X)**2+Y**2) / pump_width**2)
 pump = 1*(pump_base/np.sum(pump_base))
 cavity_modes.load_pump(pump=pump)
-cavity_modes.plot_cavity(start_mode=0, plot=False)
+cavity_modes.plot_cavity(start_mode=0, plot=True) #If plot=False, will save instead.
 
 #If you want to see more modes
 # for i in tqdm(range(1, 12)):
@@ -42,5 +43,11 @@ cavity_modes.plot_cavity(start_mode=0, plot=False)
 from PyPBEC.OpticalMedium import OpticalMedium
 QW = OpticalMedium(optical_medium="InGaAs_QW")
 absorption_rates, emission_rates = QW.get_rates(lambdas=lambdas, mode=17)
+plt.plot(lambdas*1e9, absorption_rates, label = 'absorption')
+plt.plot(lambdas*1e9, emission_rates, label = 'emission')
+plt.legend()
+plt.show()
+print('Complete')
+
 
 