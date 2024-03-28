b29e1bc85a7562cc7a6f98e799686e248056c35e
Dak <natakala.d.n@gmail.com>
2024-03-28 19:43:57 +0000

    Updates

--- Semiconductor_test.py
+++ Semiconductor_test.py
@@ -34,20 +34,83 @@ X, Y = cavity_modes.get_cavity_grid()
 pump_base = np.exp(-((X)**2+Y**2) / pump_width**2)
 pump = 1*(pump_base/np.sum(pump_base))
 cavity_modes.load_pump(pump=pump)
-cavity_modes.plot_cavity(start_mode=0, plot=True) #If plot=False, will save instead.
+cavity_modes.plot_cavity(start_mode=0, plot=False) #If plot=False, will save instead.
 
 #If you want to see more modes
 # for i in tqdm(range(1, 12)):
 #     cavity_modes.plot_cavity(start_mode=i*8, plot=False)
 
+#Set up optical medium
 from PyPBEC.OpticalMedium import OpticalMedium
 QW = OpticalMedium(optical_medium="InGaAs_QW")
 absorption_rates, emission_rates = QW.get_rates(lambdas=lambdas, mode=17)
-plt.plot(lambdas*1e9, absorption_rates, label = 'absorption')
-plt.plot(lambdas*1e9, emission_rates, label = 'emission')
-plt.legend()
-plt.show()
-print('Complete')
+
+#Normalise rates to improve performance
+cavity_loss_rate = 1.0/(10*1e-12)     # in s^-1
+
+cavity_loss_rates = np.ones(n_modes)
+emission_rates = emission_rates / cavity_loss_rate
+absorption_rates = absorption_rates / cavity_loss_rate
+
+#Some decay rate
+Gamma_down = 100.0
+
+# Properties of the photonic modes
+from PyPBEC.Cavity import Cavity
+cavity = Cavity(M=n_modes, J=g.shape[1])
+cavity.set_cavity_loss_rates(rates=cavity_loss_rates)
+cavity.set_cavity_emission_rates(rates=emission_rates)
+cavity.set_cavity_absorption_rates(rates=absorption_rates)
+
+# Properties of the molecular modes
+cavity.set_reservoir_decay_rates(rates=Gamma_down*np.ones(g.shape[1]))
+cavity.set_reservoir_pump_rates(rates=np.reshape(pump, [pump.shape[0]*pump.shape[1]]))
+cavity.set_reservoir_population(population=np.ones(cavity.J))
 
 
+# Coupling between photonic and molecular modes
+cavity.set_coupling_terms(coupling_terms=g)
 
+#Set pump values
+pump_value_min = 100.0
+pump_value_max = 1000000.0
+n_pump_values = 40
+
+delta_p = (pump_value_max/pump_value_min)**(1/n_pump_values)-1
+pumps = [(1+delta_p)**i*pump_value_min for i in range(0, n_pump_values)]
+
+
+#Solve the thing
+from PyPBEC.Solver import SteadyState
+from tqdm import tqdm
+
+populations = list()
+for value in tqdm(pumps):
+    # loads the pump onto the cavity object
+    pump = value * (pump_base / np.sum(pump_base))
+    cavity.set_reservoir_pump_rates(rates=np.reshape(pump, [pump.shape[0] * pump.shape[1]]))
+
+    # Steady-state solver
+    solver_steadystate = SteadyState(cavity_obj=cavity, ANNEALING=False)
+    # Defines the initial guess
+    if value == pumps[0]:
+        solver_steadystate.set_initial_photons(initial_photons=np.array(np.zeros(cavity.M)))
+        solver_steadystate.set_initial_excited_molecules(initial_excited_molecules=np.array(np.zeros(cavity.J)))
+    else:
+        solver_steadystate.set_initial_photons(initial_photons=np.array(solved_cavity_steadystate.photons[-1]))
+        solver_steadystate.set_initial_excited_molecules(
+            initial_excited_molecules=np.array(solved_cavity_steadystate.emols[-1]))
+
+        # Calls the solver
+    solved_cavity_steadystate = solver_steadystate.solve()
+
+    # Sums the populations over the mode degeneracy, g=n+1, with n=0,1,2,...
+    mode_degeneracy = np.array([j for j in range(0, n_modes) for i in range(0, j)][0:n_modes])
+    steady_state_photon_population = [
+        np.sum(solved_cavity_steadystate.photons[:, np.where(mode_degeneracy == mode_number)[0]], 1)
+        for mode_number in list(set(list(mode_degeneracy)))]
+    steady_state_photon_population = np.transpose(np.array(steady_state_photon_population))
+
+    # Appends
+    populations.append(steady_state_photon_population)
+populations = np.array(populations)
